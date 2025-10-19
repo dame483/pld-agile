90365ce9861599e22bb956f2f2562b3f880a4eb6
Dame DIENG <diengdame483@gmail.com>
2025-10-19 17:14:30 +0200

    Commit pour pouvoir rebase

--- src/main/java/fr/insalyon/pldagile/algorithme/MainTest.java
+++ src/main/java/fr/insalyon/pldagile/algorithme/MainTest.java
@@ -48,7 +48,6 @@ public class MainTest {
 
             double distanceTotale = 0;
             List<Chemin> chemins = tournee.getChemins();
-
             for (int i = 0; i < chemins.size(); i++) {
                 Chemin c = chemins.get(i);
                 NoeudDePassage depart = c.getNoeudDePassageDepart();
@@ -79,7 +78,6 @@ public class MainTest {
                 distanceTotale += distanceChemin;
             }
 
-            //  Résumé de la tournée
             System.out.println("=== Résumé de la tournée ===");
             System.out.printf("Distance totale : %.0f m\n", distanceTotale);
             System.out.printf("Durée totale : %.0f s\n", tournee.getDureeTotale());
