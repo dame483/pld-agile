b32172307fa3d09302fcb08421bb4aa39dba3c6f
Hélène DALON-DENEE <pipinetamusique@gmail.com>
2025-10-21 05:02:44 -0400

    Update State Design Pattern

--- src/main/java/fr/insalyon/pldagile/controleur/EtatInitial.java
+++ src/main/java/fr/insalyon/pldagile/controleur/EtatInitial.java
@@ -2,7 +2,7 @@ package fr.insalyon.pldagile.controleur;
 import fr.insalyon.pldagile.modele.Carte;
 import fr.insalyon.pldagile.modele.CarteParseurXML;
 import fr.insalyon.pldagile.modele.DemandeDeLivraison;
-import insalyon.pldagile.controleur.EtatCarteChargee;
+import fr.insalyon.pldagile.controleur.EtatCarteChargee;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
