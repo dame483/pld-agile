3161361d23058e14d20c5d0d1bd503faabca2afa
Hélène DALON-DENEE <pipinetamusique@gmail.com>
2025-10-21 07:26:38 -0400

    Update State Pattern Desing

--- src/main/java/fr/insalyon/pldagile/controleur/Etat.java
+++ src/main/java/fr/insalyon/pldagile/controleur/Etat.java
@@ -12,9 +12,9 @@ public interface Etat{
     public Carte loadCarte(Controlleur c,@RequestParam("file") MultipartFile file) ;
     public DemandeDeLivraison loadDemandeLivraison(Controlleur c, @RequestParam("file") MultipartFile file, Carte carte);
     /*public void addLivraison(Controlleur c, @RequestParam("file") MultipartFile file, Carte carte);
-    public void deleteLivraison(Controlleur c);
+    public void deleteLivraison(Controlleur c);*/
     public void runCalculTournee(Controlleur c);
-    public void saveTournee(Controlleur c);*/
+    /*public void saveTournee(Controlleur c);*/
     public Object uploadXML(String type, @RequestParam("file") MultipartFile file, Carte carte);
     public String getName();
     /*public void leftClick();
