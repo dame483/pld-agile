09c3369c4d15033ec2fa557867dd88b41a49591d
KML-Style <nathandd88@free.fr>
2025-11-03 17:11:45 +0100

    correction redondance après merge

--- src/main/java/fr/insalyon/pldagile/modele/Livraison.java
+++ src/main/java/fr/insalyon/pldagile/modele/Livraison.java
@@ -6,8 +6,6 @@ public class Livraison {
 
     public Livraison() {}
 
-    public Livraison() {}
-
     public Livraison(NoeudDePassage adresseEnlevement, NoeudDePassage adresseLivraison) {
         this.noeudEnlevement = adresseEnlevement;
         this.noeudLivraison = adresseLivraison;
